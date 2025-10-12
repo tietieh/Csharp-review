42e68a351b63a8ba8ad6e42c7f527fcecbc889a5
32267 <32267@TIETIECOMPUTER>
2025-10-12 20:49:21 +0800

    添加项目文件。

---
+++ ConsoleBasic/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleBasic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int resutl = calculate(0, 100);
+            Console.WriteLine(resutl);
+        }
+        static int calculate(int a, int b)
+        {
+            int result = 0;
+            for (int i = a; i <= b; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                    result += i;
+                }
+            }
+            return result;
+        }
+    }
+}
