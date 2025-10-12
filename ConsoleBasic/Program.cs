6e88a44a3ce921911f39da18c5f7513fac29a042
tie tie <3226748535@qq.com>
2025-10-12 23:28:48 +0800

    #位运算

--- ConsoleBasic/Program.cs
+++ ConsoleBasic/Program.cs
@@ -4,21 +4,29 @@
     {
         static void Main(string[] args)
         {
-            int resutl = calculate(0, 100);
-            Console.WriteLine(resutl);
+            //int resutl = Calculate(0, 100);
+            //Console.WriteLine(resutl);
+            BitCalculate(1, 2);
         }
-        static int calculate(int a, int b)
+        //接受两个整数参数a和b，计算从a到b之间所有能被3整除的数的和
+        static int Calculate(int a, int b)
         {
             int result = 0;
             for (int i = a; i <= b; i++)
             {
                 if (i % 3 == 0)
                 {
-                    Console.WriteLine(i);
                     result += i;
                 }
             }
             return result;
+        }
+        //位运算
+        static void BitCalculate(int a, int b)
+        {
+            Console.WriteLine(a & b); //按位与
+            Console.WriteLine(a | b); //按位或
+            Console.WriteLine(a ^ b); //按位异或
         }
     }
 }
