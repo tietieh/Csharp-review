e8a9ae02d75476de5b4a7502bee9352312734cae
tie tie <3226748535@qq.com>
2025-10-13 18:47:22 +0800

    #修改测试

--- ConsoleBasic/Program.cs
+++ ConsoleBasic/Program.cs
@@ -4,9 +4,26 @@
     {
         static void Main(string[] args)
         {
-            //int resutl = Calculate(0, 100);
-            //Console.WriteLine(resutl);
+            //调用Calculate方法
+            int resutl = Calculate(0, 100);
+            Console.WriteLine(resutl);
+            //调用BitCalculate方法
             BitCalculate(1, 2);
+            //条件运算符表达式
+            int a = 3,b = 5, c = 7;
+            if ( a < b && b < c || a > c)
+            {
+                Console.WriteLine("true");
+            }
+            //移位运算
+            int[] arr = {1, 2, 3, 4, 5 };
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] <<= 2; //左移两位
+                Console.WriteLine(arr[i]);
+            }
+
+             
         }
         //接受两个整数参数a和b，计算从a到b之间所有能被3整除的数的和
         static int Calculate(int a, int b)
