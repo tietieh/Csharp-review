95597a8d8862b3169f68f9a33ba78791738159c1
tie tie <3226748535@qq.com>
2025-10-15 10:17:40 +0800

    #接口

---
+++ ConsoleInterface/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace ConsoleInterface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+            myClass.MyMethod();
+            MyClassOne myClassOne = new MyClassOne();
+            myClassOne.ParentInterfaceMethod();
+            myClassOne.MyMethodOne();
+        }
+    }
+    //接口
+    interface IMyInterface
+    {
+        void MyMethod();
+    }
+    //继承接口的类
+    class MyClass : IMyInterface
+    {
+        public void MyMethod()//实现接口方法
+        {
+            Console.WriteLine("MyMethod called!");
+        }
+    }
+
+    //主接口
+    interface IParentInterface
+    {
+        void ParentInterfaceMethod();
+    }
+    //继承主接口的接口
+    interface IMyInterfaceOne : IParentInterface
+    {
+        void MyMethodOne();
+    }
+    //实现接口的类
+    class MyClassOne : IMyInterfaceOne
+    {
+        public void ParentInterfaceMethod()
+        {
+            Console.WriteLine("ParentInterfaceMethod called!");
+        }
+        public void MyMethodOne()
+        {
+            Console.WriteLine("MyMethodOne called!");
+        }
+    }
+    
+}
