2152fb5be782bed218d001fb565c9965a04ff331
tie tie <3226748535@qq.com>
2025-10-16 13:01:52 +0800

    #委托打印文件和屏幕输出

--- ConsoleDelegates/Program.cs
+++ ConsoleDelegates/Program.cs
@@ -3,27 +3,30 @@ namespace ConsoleDelegates
 {
     internal class Program
     {
+        //定义委托
         delegate int NumberChanger(int n);
         static void Main(string[] args)
         {
+            //实例化委托
             NumberChanger nc;
             NumberChanger nc1 = new NumberChanger(AddNum);
             NumberChanger nc2 = new NumberChanger(MultNum);
+            //委托多播
             nc = nc1;
             nc += nc2;
             nc(2);
             Console.WriteLine(getNum());
             //Func委托
-            int a;
-            int b;
+            int a = 2;
+            int b = 3;
             Func<int, int, int> add = new Func<int, int, int>(Add);
-            Console.WriteLine(add(2, 3));
-
+            Console.WriteLine(add(a, b));
+            //Action委托
             Action<string> action = (s) => Console.Write(s);
             action += (s) => Console.Write(" world");
             action("Hello");
         }
-
+        //Func委托的实现方法
         static int Add(int a, int b)
         {
             return a + b;
@@ -32,7 +35,7 @@ namespace ConsoleDelegates
         {
             return a * b;
         }
-
+        //定义nc委托的实现方法
         static int num = 10;
         public static int AddNum(int p)
         {
---
+++ ConsoleDelegates2/Program.cs
@@ -0,0 +1,48 @@
+﻿using static ConsoleDelegates2.Program;
+
+namespace ConsoleDelegates2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintString ps1 = WriteToScreen;
+            PrintString ps2 = WriteToFile;
+            sendString(ps1, "Hello World!");
+            sendString(ps2, "This is a test string.");
+        }
+        //文件流
+        static FileStream fs;
+        static StreamWriter sw;
+        //委托定义
+        public delegate void PrintString(string message);
+        //打印到屏幕
+        static void WriteToScreen(string message)
+        {
+            Console.WriteLine("The String is: {0}",message);
+        }
+        //打印到文件
+        static void WriteToFile(string message)
+        {
+            if (fs == null)
+            {
+                fs = new FileStream("D:\\Desktop\\Test.txt", FileMode.OpenOrCreate, FileAccess.Write);
+                sw = new StreamWriter(fs);
+                sw.WriteLine(message);
+                sw.Close();
+                fs.Close();
+            }
+            else
+            {
+                sw.WriteLine(message);
+                sw.Close();
+                fs.Close();
+            }
+        }
+        // 该方法把委托作为参数，并使用它调用方法
+        public static void sendString(PrintString ps, string message)
+        {
+            ps(message);
+        }
+    }
+}
