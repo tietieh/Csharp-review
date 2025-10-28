2f9e180561885fb7aa28bd4ebbc65a84382cabbe
tie tie <3226748535@qq.com>
2025-10-28 23:07:31 +0800

    #OfType测试

--- ConsoleLINQTest/Program.cs
+++ ConsoleLINQTest/Program.cs
@@ -4,6 +4,12 @@
     {
         static void Main(string[] args)
         {
+            OfTypeTest();
+
+        }
+        //Where测试
+        static void WhereTest()
+        {
             List<int> lists = new List<int>() { 1, 2, 3, 4, 5 };
             //使用LINQ查询（查询表达式语法）
             var numbers = from l in lists
@@ -15,7 +21,19 @@
             }
             //使用LINQ查询（方法语法）
             lists.Where(x => x % 2 == 0).ToList().ForEach(x => Console.WriteLine(x));
-
+        }
+        //OfType测试
+        static void OfTypeTest()
+        {
+            List<object> lists = new List<object>() { 1, "2", 3, "4", 5 };
+            //使用OfType查询(查询表达式语法)
+            var numbers = lists.OfType<int>();
+            foreach (var num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+            //使用OfType查询(方法语法)
+            lists.OfType<string>().ToList().ForEach(x => Console.WriteLine(x));
         }
     }
 }
